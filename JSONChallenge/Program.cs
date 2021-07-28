be4ea09be39083b8344b5acf88eff23bd90e2690
Shelby Simpson <shelbycam78@gmail.com>
2021-07-27 20:57:38 -0400

    challenge 1 done

---
+++ JSONChallenge/Program.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person person = new Person();
+
+            person.FirstName = "Phil";
+            person.LastName = "McCrackin";
+            person.DOB = new DateTime(1994, 10, 2);
+            person.FavoriteIceCreamFlavors = new List<string>
+            {"strawberry", "Cookie Dough"
+            };
+            string json = JsonConvert.SerializeObject(person);
+
+            Console.WriteLine(json);
+
+            Console.ReadKey();
+
+        }
+    }
+}
