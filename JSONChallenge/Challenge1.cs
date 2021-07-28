da024d665cdae875193a724b62b1155ec8fba7a9
Shelby Simpson <shelbycam78@gmail.com>
2021-07-27 21:06:27 -0400

    challenge 2 done

--- JSONChallenge/Challenge1.cs
+++ JSONChallenge/Challenge1.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace JSONChallenge
 {
-    class Challenge1
+    public class Challenge1
     {
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public string EventTitle { get; set; }
+        public double EventCapacity { get; set; }
+        public decimal PriceOfAdmission { get; set; }
+
     }
 }
--- JSONChallenge/Challenge2.cs
+++ JSONChallenge/Challenge2.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace JSONChallenge
 {
-    class Challenge2
+    public class Challenge2
     {
+        public int BadgeID { get; set; }
+
+        public List<string> DoorList { get; set; }
+
+        public bool HasSecurityClearance { get; set; }
+
+        public string EmployeeId { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+
     }
 }
